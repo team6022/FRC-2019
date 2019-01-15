699569c2d955475e27b83dd23d6c5e5a8f755aea
Matt Steiner <msteiner@ibidata.com>
2019-01-14 20:22:11 -0600

    started using RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,16 @@
 package frc.robot;
 
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around
+ */
 public class RobotMap
 {
+
+	// VelcroPistons Channels
+	public static final int VelcroPistonsForwardChannel = 0;
+	public static final int VelcroPistonsReverseChannel = 1;
+
 }
--- src/main/java/frc/robot/subsystems/VelcroPistons.java
+++ src/main/java/frc/robot/subsystems/VelcroPistons.java
@@ -2,22 +2,22 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.*;
 
 public class VelcroPistons extends Subsystem {
 
-	static DoubleSolenoid doubleSolenoid = new DoubleSolenoid(0, 1);
+	static DoubleSolenoid doubleSolenoid = new DoubleSolenoid(RobotMap.VelcroPistonsForwardChannel, RobotMap.VelcroPistonsReverseChannel);
 
 	public VelcroPistons()
 	{
 		super();
 	}
 
-		public void initDefaultCommand()
-		{
+	public void initDefaultCommand()
+	{
 
-		}
+	}
 
 	public void PistonOut(Joystick Branjoy)
 	{
