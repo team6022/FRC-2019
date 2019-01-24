553414c025ef9c497cb9f6adceba0f936a500544
Wrench Warmers <wrenchwarmers6022@gmail.com>
2019-01-24 17:15:27 -0600

    added buttons for lift pistons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,12 +11,14 @@ public class OI
 	final Joystick Branjoy = new Joystick(0);
 	final Joystick Hank = new Joystick(1);
 	Button button1 = new JoystickButton(Branjoy, 1);
-	Button button2 = new JoystickButton(Branjoy, 2);
+	Button button3 = new JoystickButton(Branjoy, 3);
+	Button button4 = new JoystickButton(Branjoy, 4);
 
 	public OI()
 	{
 		button1.whenPressed(new VelcroPistonsToggle());
-		// button2.whenPressed(new VelcroPistonsIn());
+		button3.whenPressed(new LiftPistonsIn());
+		button4.whenPressed(new LiftPistonsOut());
 	}
 
 	public Joystick getJoystickBran()
